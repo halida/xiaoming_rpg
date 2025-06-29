5b96e079f4114f1f87905e36f4892a2d2fbafc80
halida <linjunhalida@gmail.com>
2025-06-29 10:23:13 +0800

    life

--- player.js
+++ player.js
@@ -1,8 +1,13 @@
-export class Player {
-    constructor(name = 'Xiaoming', hp = 100, mp = 100) {
-        this.name = name;
-        this.hp = hp;
-        this.mp = mp;
-    }
+export class Life {
+  constructor(name, hp, mp) {
+    this.name = name;
+    this.hp = hp;
+    this.mp = mp;
+  }
 }
 
+export class Player extends Life {
+  constructor(){
+    super('Xiaoming', 100, 100);
+  }
+}
