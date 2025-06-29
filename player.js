fa1d8ee54b836193decd87ddd248766e526c5d05
halida <linjunhalida@gmail.com>
2025-06-29 10:10:17 +0800

    add player

---
+++ player.js
@@ -0,0 +1,8 @@
+export class Player {
+    constructor(name = 'Xiaoming', hp = 100, mp = 100) {
+        this.name = name;
+        this.hp = hp;
+        this.mp = mp;
+    }
+}
+
--- run.js
+++ run.js
@@ -1,4 +1,8 @@
+import { Player } from './player.js';
 
+const player = new Player();
 
-
-console.log('Hello');
+console.log('Player Attributes:');
+console.log(`  Name: ${player.name}`);
+console.log(`  HP: ${player.hp}`);
+console.log(`  MP: ${player.mp}`);
